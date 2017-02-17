2e0146eb2093cf2f40b6cb79a8644c4dbecea3a3
chriskottom <chris@chriskottom.com>
2017-02-17 10:46:40 +0100

    Create all basic models in one go
    They are: User, Board, List, Card, Comment.  Basic Rails generators
    did most of the work here, but I updated the migrations to add the
    non-standard foreign key constraints correctly and the models to
    configure object relationships and validations appropriately.

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  has_many :boards, foreign_key: 'creator_id', dependent: :nullify
+  has_many :lists, foreign_key: 'creator_id', dependent: :nullify
+  has_many :cards, foreign_key: 'creator_id', dependent: :nullify
+  has_many :comments, foreign_key: 'creator_id', dependent: :nullify
+  has_many :assigned_cards, dependent: :nullify,
+           class_name: 'Card', foreign_key: 'assignee_id'
+
+  validates :email, presence: true, uniqueness: true
+end
