c4c4c25de8a95bba4ee1e05581d09c9c3acab1df
chriskottom <chris@chriskottom.com>
2017-02-18 17:09:33 +0100

    Add a :links attribute to the serialized User
    We include a new attribute for passing along the URL for accessing
    the serialized User model directly as well as the link for all
    Boards belonging to the User.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,16 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :gravatar_url, :created_at, :updated_at
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :email, :gravatar_url, :created_at, :updated_at, :links
+
+  def links
+    [
+      link(:self, user_url(object)),
+      link(:boards, user_boards_url(object))
+    ]
+  end
+
+  def link(rel, href)
+    { rel: rel, href: href }
+  end
 end
