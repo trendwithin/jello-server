add31ab3743c47244268572efeaecb66e310b2f7
chriskottom <chris@chriskottom.com>
2017-02-27 13:08:02 +0100

    Customize 401 Unauthorized response
    To get error responses generated by Knock for both bad credentials
    and invalid / missing authorization tokens to conform to our usual
    error format:
    - Extract the various error response methods to a concern.
    - Include that concern in both the ApplicationController and the
      Knock::AuthTokenController subclass.
    - Ensure that exceptions thrown during login are caught by an
      appropriate rescue_from hook.

--- app/controllers/v2/base_controller.rb
+++ app/controllers/v2/base_controller.rb
@@ -2,4 +2,10 @@ class V2::BaseController < ApplicationController
   include Knock::Authenticable
 
   before_action :authenticate_user
+
+  private
+
+  def unauthorized_entity(entity)
+    respond_with_unauthorized
+  end
 end
