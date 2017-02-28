8357b63484664c551fad1a9f04d2d31d9138f155
chriskottom <chris@chriskottom.com>
2017-02-28 10:44:05 +0100

    Require HTTP digest authentication for all endpoints
    Protect all controller actions with mandatory HTTP digest authentication.
    The username and password should be stored as environment variables,
    and the requirement to authenticate should be conditional based on the
    presence of the username variable.

--- app/controllers/v2/base_controller.rb
+++ app/controllers/v2/base_controller.rb
@@ -1,2 +1,15 @@
 class V2::BaseController < ApplicationController
+  include ActionController::HttpAuthentication::Digest::ControllerMethods
+
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    if ENV['AUTHORIZED_USERNAME']
+      authenticate_or_request_with_http_digest do |username|
+        ENV['AUTHORIZED_PASSWORD'] if ENV['AUTHORIZED_USERNAME'] == username
+      end
+    end
+  end
 end
